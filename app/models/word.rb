9eb53d406d1318bed7b5560e58f9c25978c577f2
Mark Azuolas <markazuolas@gmail.com>
2017-08-24 11:51:11 -0700

    add word validations and method outline

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,3 +1,11 @@
 class Word < ApplicationRecord
+  validates :word, :def0 presence: true
 
+  def self.definition(word)
+    #return definition if there is one
+  end
+
+  def self.add_fake_word(word)
+    #add word to fake word DB
+  end
 end
