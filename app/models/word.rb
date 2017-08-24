48739ab1f21d2275eee97f622719d7af3911218b
Mark Azuolas <markazuolas@gmail.com>
2017-08-24 14:26:33 -0700

    add definition creations for fake words

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,12 +1,30 @@
 class Word < ApplicationRecord
   validates :word, :def0, presence: true
 
+  def definitions
+    defs = [self.def0]
+    defs << def1 if def1
+    defs << def2 if def2
+    defs << def3 if def3
+    defs
+  end
+
   def self.definition(word)
     #return definition if there is one
-    Wordnik.word.get_definitions(word)
+    definitions = Wordnik.word.get_definitions(word).map {|wordnik| wordnik["text"] }
+    if definitions.empty?
+      fake_word = Word.find_by(word: word.downcase)
+      definitions = fake_word ? fake_word.definitions : []
+    end
+
+    definitions
   end
 
   def self.add_fake_word(word)
-    #add word to fake word DB
+    #add word to fake word DB or update time
+    fake_word = Word.new
+    fake_word.word = word.downcase
+    fake_word.def0 = LiterateRandomizer.sentence
+    fake_word.save
   end
 end
