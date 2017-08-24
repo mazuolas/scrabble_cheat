c018dfd2c5a52215cf64cd0487429253909224be
Mark Azuolas <markazuolas@gmail.com>
2017-08-24 14:29:40 -0700

    add check for leading space on search query

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,10 @@ class PagesController < ApplicationController
 
   def search
     @searched_word = search_params[:query]
+    if @searched_word[0] == " "
+      @searched_word = @searched_word[1..-1]
+      Word.add_fake_word(@searched_word)
+    end
     @definitions = Word.definition(@searched_word)
     if @definitions.empty?
       render :word_not_found
